f1adabf15be34dbb645c3ea3712cac19329fc79f
Jeremy Quinn <jeremy.q97@gmail.com>
2017-08-20 01:46:33 -0400

    Initial commit
    I started this up a couple days ago and decided I should put it up on
    here. Basically working out packet encoding and decoding currently, then
    plan to move onto Socket work

---
+++ TestClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the test client.");
+            Console.Title = "Test Client";
+            while (true)
+            {
+                Console.WriteLine("Sending new coordinates to server:");
+                Random r = new Random();
+                float x = (float)r.NextDouble();
+                float y = (float)r.NextDouble();
+                float z = (float)r.NextDouble();
+            }
+        }
+    }
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the test server.");
+            Console.Title = "Test Server";
+
+            try
+            {
+                IPAddress address = IPAddress.Parse("96.32.44.229");
+                TcpListener listener = new TcpListener(address, 8001);
+                listener.Start();
+                Console.WriteLine("Listener running on port 8001");
+
+                Socket s = listener.AcceptSocket();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            while (true)
+            {
+                Console.Write("Continue running server? (y/n): ");
+                string input = Console.ReadLine();
+                if (input.ToLower().Equals("n"))
+                {
+                    return;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
